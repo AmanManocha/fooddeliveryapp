704d038c2321d0b4be7977ce16a27ae57c697d48
AmanManocha <aman.manocha@netsmartz.net>
2022-02-25 17:18:19 +0530

    first step done

---
+++ src/components/restaurant.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Dropdown,DropdownButton} from 'react-bootstrap'
+
+export default function restaurant() {
+  return (
+    <DropdownButton id="dropdown-item-button" title="Dropdown button">
+  <Dropdown.Item as="button" herf="">Dominos</Dropdown.Item>
+  <Dropdown.Item as="button">Burger King</Dropdown.Item>
+  <Dropdown.Item as="button">Macdonalds</Dropdown.Item>
+  <Dropdown.Item as="button" herf="">pizza hut</Dropdown.Item>
+  <Dropdown.Item as="button">starbucks</Dropdown.Item>
+</DropdownButton>
+  )
+}
